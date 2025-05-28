59e63717ca3d635974b34d318a57961844e9a561
dxsw <yurabakai2.0@gmail.com>
2025-05-28 14:03:12 +0200

    feat: add mongodb repository

--- rest_course/api/books.py
+++ rest_course/api/books.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, HTTPException
 
 from ..models.book import Book, BookCreate
-from ..storage.memory import get_all_books, get_book_by_id, add_book, delete_book
+from ..storage.mongo import get_all_books, get_book_by_id, add_book, delete_book
 
 
 router = APIRouter()
@@ -15,7 +15,7 @@ async def list_books():
 
 
 @router.get("/{book_id}", response_model=Book)
-async def get_book(book_id: int):
+async def get_book(book_id: str):
     book = await get_book_by_id(book_id)
     if not book:
         raise HTTPException(status_code=404, detail="Book not found")
@@ -28,6 +28,6 @@ async def create_book(book_data: BookCreate):
 
 
 @router.delete("/{book_id}", status_code=204)
-async def remove_book(book_id: int):
+async def remove_book(book_id: str):
     if not await delete_book(book_id):
         raise HTTPException(status_code=404, detail="Book not found")
